d1dbf1bf14e513a052506f66620491206389c99f
Aquila <aquilahenrique.silva@gmail.com>
2020-12-26 19:22:08 -0300

    projeto concluido

---
+++ script.js
@@ -0,0 +1,40 @@
+
+ 
+// iniciando os inputs 
+const main_temp = document.querySelector(".celsius");
+const cidade = document.querySelector(".cidade")
+const condicao = document.querySelector(".condicao")
+const min_temp = document.querySelector(".min-tem")
+const max_temp = document.querySelector(".max-tem")
+const icone = document.querySelector(".icon img")
+
+
+// função que vai renderizar os dados no html
+let render = (data) => {
+    console.log(data)
+    main_temp.innerHTML = data.temp + "° C"
+    cidade.innerHTML = data.city
+    condicao.innerHTML = data.description
+    min_temp.innerHTML = data.forecast[0].min + "° C"
+    max_temp.innerHTML = data.forecast[0].max + "° C"
+    icone.src = `http://assets.api.hgbrasil.com/weather/images/${data.img_id}.png`
+    
+}
+
+function buscarDados(lat, long) {
+    const url = `https://api.hgbrasil.com/weather?format=json-cors&key=91f0c9df&lat=${lat}&lon=${long}&user_ip=remote`;
+
+    fetch(url).then(Response => {
+        return Response.json()
+            }).then(api_data =>{
+                render(api_data.results)
+                }).catch(error => console.log(error))
+}
+// buscando a geolocalização pela api do Browser 
+navigator.geolocation.getCurrentPosition((position)=> {
+    lat = position.coords.latitude
+     long = position.coords.longitude
+     buscarDados(lat, long)
+ })
+
+ console.log(icone.img)
